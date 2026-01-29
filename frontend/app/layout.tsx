8cbaf55f654a56b949ba33050f92f0f11f4dd7ce
Piotr Synak <173157692+kotsinus@users.noreply.github.com>
2026-01-29 22:34:21 +0100

    frontend dockerized

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -32,8 +32,8 @@ export default function RootLayout({
   children: ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
